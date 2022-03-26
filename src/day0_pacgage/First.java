878b6f2896e4776daca2c3fc43caaed2b95fafd2
Dusan Milivojevic <Dusanmilivojevic.sb@gmail.com>
2022-03-26 13:14:55 -0700

    updated first file

--- src/day0_pacgage/First.java
+++ src/day0_pacgage/First.java
@@ -1,7 +1,10 @@
 package day0_pacgage;
 
 public class First {
-
+    public static void main(String[] args) {
+        System.out.println("Java");
+        System.out.println("Working");
+    }
     //TODO: Works on this tommorow
     // Add some cool code
 
